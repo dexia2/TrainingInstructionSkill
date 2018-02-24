d3bd297f2eedebc4700ae4092bae5f3e1e11a5a8
dexia2 <n-koba@fis.nachi-fujikoshi.co.jp>
2018-02-24 19:32:17 +0900

    add ask mode

--- index.js
+++ index.js
@@ -1,38 +1,66 @@
 "use strict";
-var Alexa = require('alexa-sdk'); // Alexa SDKの読み込み
+var Alexa = require('alexa-sdk');
 
+var STOP_MESSAGE = 'さようなら';
+var HELP_MESSAGE = '次！とつぶやいてください';
+var states = {
+    PLAYING_MODE: '_PLAYING_MODE'
+};
 var strokes = [
     "カットが来ました",
     "スマッシュが来ました。",
     "クリアが来ました。"
 ];
-
-var handlers = {
-        'LaunchRequest': function() {
-            this.emit('GoNextStroke');
-        },
-        'GoNextStroke': function() {
-            var strokeIndex = Math.floor(Math.random() * strokes.length);
-            this.emit(':tell', strokes[strokeIndex]);
-        },
-        'AMAZON.HelpIntent': function() {
-            this.emit(':tell', '次のストローク！とつぶやいてください。');
-        },
-        'AMAZON.StopIntent': function() {
-            this.emit(':tell', 'さようなら');
-        },
-        'AMAZON.CancelIntent': function() {
-            this.emit(':tell', 'さようなら');
-        }
+var commands = [
+    '次へ',
+    '次',
+    'ゴー',
+    'つぎ'
+];
+var nextStroke = function () {
+    return strokes[Math.floor(Math.random() * strokes.length)];
 };
 
-exports.handler = function(event, context, callback) {
+var startHandlers = {
+    'LaunchRequest': function () {
+        this.handler.state = states.PLAYING_MODE;
+        this.emit(':ask', nextStroke());
+    },
+    'AMAZON.HelpIntent': function () {
+        this.emit(':tell', HELP_MESSAGE);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    }
+};
 
+var goNextHandler = Alexa.CreateStateHandler(states.PLAYING_MODE,  {
+    'GoNextIntent': function () {
+        const command = this.event.request.intent.slots.command.value;
+        if (commands.indexOf(command) > -1) {
+            this.emit(':ask', nextStroke());
+        } else {
+            this.handler.state = '';
+            this.emit(':tell', STOP_MESSAGE);
+        }
+    },
+    'AMAZON.HelpIntent': function () {
+        this.emit(':tell', HELP_MESSAGE);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    }
+});
+
+exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
-
-    // Alexa SDKの処理
     alexa.appId = process.env.APP_ID;
-    alexa.registerHandlers(handlers);
+    alexa.registerHandlers(startHandlers, goNextHandler);
     alexa.execute();
-
 };
